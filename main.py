15baca311081a33039d17b8d05e143a8a8c0640b
Jacob Ayinsakia <jacob.ayinsakia@hubtel.com>
2025-01-15 17:40:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,20 @@ while True:
     match user_action:
         case 'add':
             todo = input("Enter a todo: ") + "\n"
+
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
             file =open('todos.txt', 'w')
             file.writelines(todos)
+            file.close()
         case 'show':
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+            
             for index, item in enumerate(todos):
                 row = f"{index + 1}-{item}"
                 print(row)
