72f67f2aaa85242ca0dc836df23f14d3592c742c
Jacob Ayinsakia <jacob.ayinsakia@hubtel.com>
2025-02-18 20:20:12 +0000

    Optimzd code with doc strings

---
+++ Bonus/File/bouns13.py
@@ -0,0 +1,22 @@
+feet_inches = input("Enter feet and inches: ")
+
+def parse(feet_inches):
+    parts = feet_inches.split(" ")
+    feet = float(parts[0])
+    inches = float(parts[1])
+    return {"feet": feet, "inches": inches}
+
+def convert(feet, inches):
+    meters = feet * 0.344 + inches * 0.055
+    return meters
+
+parsed = parse(feet_inches)
+
+result = convert(parsed['feet'], parsed['inches'])
+
+print(f"{parsed['feet']} feet and {parsed['inches']} is equal to {result}")
+
+if result < 1:
+    print("kid is too small")
+else:
+    print("kid can use slide")
