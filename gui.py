617f72f83c06888a9f6b6212dd115475daa76302
Jacob Ayinsakia <jacob.ayinsakia@hubtel.com>
2025-03-08 03:44:01 +0000

    Added new features

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ label = sg.Text("Type in a to-do")
 input_box =sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
-                      enable_events=True, size=[45, 10])
+                      enable_events=True, size=[40, 10])
 edit_button = sg.Button("Edit")
 
 window = sg.Window('My To-Do App',
@@ -14,15 +14,15 @@ window = sg.Window('My To-Do App',
 
 while True:
     event, values = window.read()
-    print(1, event)
-    print(2, values)
-    print(3, values['todos'])
+    print(values)
     match event:
         case "Add":
             todos = functions.get_todos()
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
         case "Edit":
             todo_to_edit = values['todos'][0]
             new_todo = values['todo']
@@ -33,7 +33,7 @@ while True:
             functions.write_todos(todos)
             window['todos'].update(values=todos)
         case 'todos':
-            window['todo'].update(values)
+            window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
 
